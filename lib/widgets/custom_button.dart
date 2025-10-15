dd2231abc17253dbab7d42e1c4da353e35f65c3a
Ziad Tamer <ztamer494@gmail.com>
2025-10-15 17:49:18 +0300

    finish notes buttom sheet

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+
+class CutomButton extends StatelessWidget {
+  const CutomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+
+      height: 55,
+      decoration: BoxDecoration(
+        color: kPrimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 22,
+          ),
+        ),
+      ),
+    );
+  }
+}
