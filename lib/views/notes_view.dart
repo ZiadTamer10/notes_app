69b2d75030d8e446a27331d0978e9ee7d4cdadc7
Ziad Tamer <ztamer494@gmail.com>
2025-10-19 15:49:59 +0300

    refactor blocProvider place

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/constants.dart';
-import 'package:notes_app/cubits/read_note/read_note_cubit.dart';
 import 'package:notes_app/widgets/add_note_buttom_sheet.dart';
 import 'package:notes_app/widgets/notes_view_body.dart';
 
@@ -10,25 +8,22 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => ReadNoteCubit(),
-      child: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          shape: CircleBorder(),
-          backgroundColor: kPrimaryColor,
-          onPressed: () {
-            showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return AddNoteButtomSheet();
-              },
-            );
-          },
-          child: Icon(Icons.add, color: Colors.black),
-        ),
-        body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        shape: CircleBorder(),
+        backgroundColor: kPrimaryColor,
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) {
+              return AddNoteButtomSheet();
+            },
+          );
+        },
+        child: Icon(Icons.add, color: Colors.black),
       ),
+      body: NotesViewBody(),
     );
   }
 }
